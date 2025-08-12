bdddc15decc4ec1d7cf4acee03dafecf8fc30b38
pranav <Pranav.Srinivasula@vibhotech.com>
2025-08-12 17:02:57 +0530

    add full project files

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ const app = express();
 
 
 
-app.use(express.json({
-  verify: (req, res, buf) => { req.rawBody = buf.toString(); }, // needed for signature verification
-}));
+// app.use(express.json({
+//   verify: (req, res, buf) => { req.rawBody = buf.toString(); }, // needed for signature verification
+// }));
 
-app.use("/", flowRoutes);
+app.use('/flow-webhook', express.raw({ type: '*/*' }), flowRouter);
+
+// Other parsers for normal routes
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
