2b3d12fa8c343a020acc6d90476d42332dc4b0cb
pranav <Pranav.Srinivasula@vibhotech.com>
2025-08-12 16:53:41 +0530

    add full project files

--- Routes/routes.js
+++ Routes/routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const { flowWebhook } = require("../controllers/sendDropdown");
 
-router.post("/flow-webhook", flowWebhook);
+// router.post("/flow-webhook", flowWebhook);
+app.post("/flow-webhook", express.raw({ type: "*/*" }), flowWebhook);
 
 module.exports = router;
