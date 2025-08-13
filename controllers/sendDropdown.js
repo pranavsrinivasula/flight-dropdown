f56c484ffa9ed738dc343608a712aef447341691
pranav <Pranav.Srinivasula@vibhotech.com>
2025-08-13 15:42:01 +0530

    add full project files

--- controllers/sendDropdown.js
+++ controllers/sendDropdown.js
@@ -83,36 +83,39 @@ const getNextScreen = async (decryptedBody) => {
 //   }
 // };
 const flowWebhook = async (req, res) => {
-    if (!PRIVATE_KEY) {
-        throw new Error(
-            'Private key is empty. Please check your env variable "PRIVATE_KEY".'
-        );
-    }
+  if (!PRIVATE_KEY) {
+    throw new Error('Private key is empty. Please check your env variable "PRIVATE_KEY".');
+  }
 
-    if (!isRequestSignatureValid(req)) {
-        // Return status code 432 if request signature does not match.
-        // To learn more about return error codes visit: https://developers.facebook.com/docs/whatsapp/flows/reference/error-codes#endpoint_error_codes
-        return res.status(432).send();
-    }
+  if (!isRequestSignatureValid(req)) {
+    return res.status(432).send();
+  }
 
-    let decryptedRequest = null;
-    try {
-        decryptedRequest = decryptRequest(req.body, PRIVATE_KEY, PASSPHRASE);
-    } catch (err) {
-        console.error(err);
-        if (err instanceof FlowEndpointException) {
-            return res.status(err.statusCode).send();
-        }
-        return res.status(500).send();
-    }
+  let encryptedBody;
+  try {
+    // req.body might be Buffer if we used express.raw()
+    encryptedBody = Buffer.isBuffer(req.body)
+      ? JSON.parse(req.body.toString("utf8"))
+      : req.body;
 
+    const decryptedRequest = decryptRequest(encryptedBody, PRIVATE_KEY, PASSPHRASE);
     const { aesKeyBuffer, initialVectorBuffer, decryptedBody } = decryptedRequest;
+
     console.log("💬 Decrypted Request:", decryptedBody);
 
     const screenResponse = await getNextScreen(decryptedBody);
     console.log("👉 Response to Encrypt:", screenResponse);
 
-    res.send(encryptResponse(screenResponse, aesKeyBuffer, initialVectorBuffer));
-}
+    const encryptedResponse = encryptResponse(screenResponse, aesKeyBuffer, initialVectorBuffer);
+    res.send(encryptedResponse);
+
+  } catch (err) {
+    console.error(err);
+    if (err instanceof FlowEndpointException) {
+      return res.status(err.statusCode).send();
+    }
+    return res.status(500).send();
+  }
+};
 
 module.exports = { flowWebhook, getNextScreen };
