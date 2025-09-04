8b64b41008c815a9c6d1df04af359429a3750d38
pranav <Pranav.Srinivasula@vibhotech.com>
2025-09-04 13:43:15 +0530

    add full project files

--- controllers/sendDropdown.js
+++ controllers/sendDropdown.js
@@ -106,6 +106,8 @@ const getNextScreen = async (decryptedBody) => {
               cities: SCREEN_RESPONSES.FLIGHT_BOOKING_SCREEN.cities,
               calendar: { min_date: formatDate(today), max_date: formatDate(maxDate) },
               enddate_visible: { value: !!data.Startdate },
+               is_age_enabled: false,
+  is_to_city_enabled: false,
               error: err.message
             }
           };
