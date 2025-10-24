591e69bacecfa8c037f6ba8ec05b22b3dd5646b2
pranav <Pranav.Srinivasula@vibhotech.com>
2025-10-24 11:51:55 +0530

    new code .Optin

--- controllers/flightChipsController.js
+++ controllers/flightChipsController.js
@@ -1,14 +1,14 @@
 // controllers/flightChipsController.js
 
 // Initial chip states
-let flightChips = [
+let Flight_Type = [
   { id: "1", title: "One-Way", selected: false, enabled: true },
   { id: "2", title: "Return", selected: false, enabled: true }
 ];
 
 // GET current chip states
 exports.getChips = (req, res) => {
-  res.json({ chips: flightChips });
+  res.json({ chips: Flight_Type });
 };
 
 // POST select/deselect a chip
@@ -19,7 +19,7 @@ exports.toggleChip = (req, res) => {
     return res.status(400).json({ error: "selectedId is required" });
   }
 
-  flightChips = flightChips.map(chip => {
+  flightChips = Flight_Type.map(chip => {
     if (chip.id === selectedId) {
       // Toggle the clicked chip
       return { ...chip, selected: !chip.selected, enabled: true };
